5de26941676145b3820755879e7af080d22e0b63
LinoyYarkoni <linoyarkoni20@gmail.com>
2023-06-29 19:28:19 +0300

    Added DataManager Class Library.
    Connect User Controller to DB.

---
+++ DataManager.Library/DataAccess/UserData.cs
@@ -0,0 +1,19 @@
+﻿using DataManager.Library.Internal.DataAccess;
+using DataManager.Library.Models;
+using System.Collections.Generic;
+
+namespace DataManager.Library.DataAccess
+{
+    public class UserData
+    {
+        public List<UserModel> GetUserById(string id)
+        {
+            SqlDataAccess sql = new SqlDataAccess();
+            var parameters = new { Id = id };
+
+            var output = sql.LoadData<UserModel, dynamic>("dbo.spUserLookup", parameters, "RMData");
+
+            return output;
+        }
+    }
+}
