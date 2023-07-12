ade9343c6a39aec11f76e56e0c4168f1de365f39
LinoyYarkoni <linoyarkoni20@gmail.com>
2023-07-12 23:58:47 +0300

    Add additional Item Info
    Adds item quantity and price to the SaleVIew

--- RMDesktopUI/Views/ShellView.xaml.cs
+++ RMDesktopUI/Views/ShellView.xaml.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
@@ -21,6 +24,12 @@ namespace RMDesktopUI.Views
     {
         public ShellView()
         {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("he-IL");
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("he-IL");
+            LanguageProperty.OverrideMetadata(typeof(FrameworkElement),
+                new FrameworkPropertyMetadata(XmlLanguage.GetLanguage
+                (CultureInfo.CurrentCulture.IetfLanguageTag)));
+
             InitializeComponent();
         }
     }
