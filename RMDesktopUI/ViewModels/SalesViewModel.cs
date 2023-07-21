192e05b640dca32db11d38465f4a3c6aef17972a
LinoyYarkoni <linoyarkoni20@gmail.com>
2023-07-21 19:58:00 +0300

    Add Dialog Box and Check Permissions

--- RMDesktopUI/ViewModels/SalesViewModel.cs
+++ RMDesktopUI/ViewModels/SalesViewModel.cs
@@ -4,10 +4,13 @@ using RMDesktopUI.Library.Api;
 using RMDesktopUI.Library.Helpers;
 using RMDesktopUI.Library.Models;
 using RMDesktopUI.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Dynamic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace RMDesktopUI.ViewModels
 {
@@ -17,6 +20,8 @@ namespace RMDesktopUI.ViewModels
         private IProductEndpoint _productEndpoint;
         private ISaleEndpoint _saleEndpoint;
         private IMapper _mapper;
+        private IWindowManager _window;
+        private StatusInfoViewModel _status;
         private BindingList<ProductDisplayModel> _products;
         private BindingList<CartItemDisplayModel> _cart = new BindingList<CartItemDisplayModel>();
         private ProductDisplayModel _selectedProduct;
@@ -27,19 +32,48 @@ namespace RMDesktopUI.ViewModels
             IProductEndpoint productEndpoint,
             IConfigHelper configHelper,
             ISaleEndpoint saleEndpoint,
-            IMapper mapper)
+            IMapper mapper,
+            StatusInfoViewModel status,
+            IWindowManager window)
         {
             _productEndpoint = productEndpoint;
             _configHelper = configHelper;
             _saleEndpoint = saleEndpoint;
             _mapper = mapper;
+            _status = status;
+            _window = window;
         }
 
         protected override async void OnViewLoaded(object view)
         {
             base.OnViewLoaded(view);
 
-            await loadProducts();
+            try
+            {
+                await loadProducts();
+            }
+            catch(Exception ex)
+            {
+                dynamic settings = new ExpandoObject();
+                settings.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+                settings.ResizeMode = ResizeMode.NoResize;
+                settings.Title = "System Error";
+
+                if(ex.Message == "Unauthorized")
+                {
+                    _status.UpdateMessage("Unauthorize Access",
+                        "You do not have a permission to interact with the sale view");
+                    await _window.ShowDialogAsync(_status, null, settings);
+                }
+                else
+                {
+                    _status.UpdateMessage("Fatal Exception", ex.Message);
+                    await _window.ShowDialogAsync(_status, null, settings);
+                }
+
+
+                await TryCloseAsync();
+            }  
         }
 
         public BindingList<ProductDisplayModel> Products
